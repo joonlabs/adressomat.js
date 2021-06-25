a38dd4831ca0a5b3673abff85f802f84e15416fa
Julius Türich <info@joonlabs.com>
2021-06-25 14:47:47 +0200

    patched last commit

--- js/adressomat.js
+++ js/adressomat.js
@@ -148,7 +148,7 @@ class _AdressOMatMap {
 
         this.map = new window.mapboxgl.Map({
             container: container,
-            style: "http://maps.adressomat.de/api/style?key=" + key,
+            style: "https://maps.adressomat.de/api/style?key=" + key,
             center: [longitude, latitude],
             zoom: zoom
         });
